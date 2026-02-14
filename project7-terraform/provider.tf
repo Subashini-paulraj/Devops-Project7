256b4cc0d46215cc98dc1648a02bf25fc7265fdd
Subashini Paulraj <subashiniraj2000@gmail.com>
2026-02-14 11:06:17 +0530

    tfstate file -backend details added

--- project7-terraform/provider.tf
+++ project7-terraform/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  backend "s3" {
+    bucket         = "project7-terraform-state"
+    key            = "project7/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
